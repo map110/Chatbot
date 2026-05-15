8f8c53d610ee8ac9dc85ae0663a22327ea4eb958
m.peykanfar <m.peykanfar@PEYKANFAR-PC>
2026-05-15 16:37:17 +0330

    افزودن vite و جداسازی فایل ها

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { ChatInput } from './components/ChatInput';
+import ChatMessages from './components/ChatMessages';
+import './App.css'
+
+
+
+function App() {
+  const [chatMessages, setChatMessages] = useState([
+    {
+      message: "hello chatbot",
+      sender: "user",
+      id: "id1",
+    },
+    {
+      message: "Hello! How can I help you?",
+      sender: "robot",
+      id: "id2",
+    },
+    {
+      message: "can you get me todays date?",
+      sender: "user",
+      id: "id3",
+    },
+    {
+      message: "Today is September 27",
+      sender: "robot",
+      id: "id4",
+    },
+  ]);
+  return (
+    <div className="app-container">
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+      <ChatMessages chatMessages={chatMessages} />
+    </div>
+  );
+}
+
+export default App;
